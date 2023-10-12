048ac9819ef29113f9177848d8eb85caec2a2f07
Paulius G <paulius@2simple.com>
2023-10-12 06:18:06 +0100

    use variable in expense component

--- starting-project-vite/src/components/ExpenseItem.tsx
+++ starting-project-vite/src/components/ExpenseItem.tsx
@@ -1,16 +1,17 @@
-// import { useState } from 'react'
 import './ExpenseItem.css';
 
 
 function ExpenseItem() {
-    //   const [count, setCount] = useState(0)
+    const date = new Date(2021, 2, 28);
+    const title = "Car Insurance";
+    const price = 252.23;
 
     return (
         <div className='expense-item'>
-            <div>March 28th 2021</div>
+            <div>{date.toDateString()}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>259.45</div>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>{price}</div>
             </div>
         </div>
     )
