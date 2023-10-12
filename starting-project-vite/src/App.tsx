f11a037785cdc3875cc94fe886c9a80570e1caec
Paulius G <paulius@2simple.com>
2023-10-12 06:00:55 +0100

    initial push starting-project

---
+++ starting-project-vite/src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+import './App.css'
+import ExpenseItem from './components/ExpenseItem'
+
+function App() {
+  //  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ExpenseItem />
+    </>
+  )
+}
+
+export default App
---
+++ starting-project-vite/src/components/ExpenseItem.tsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import './ExpenseItem.css';
+
+
+function ExpenseItem() {
+    //   const [count, setCount] = useState(0)
+
+    return (
+        <div className='expense-item'>
+            <div>March 28th 2021</div>
+            <div className='expense-item__description'>
+                <h2>Car Insurance</h2>
+                <div className='expense-item__price'>259.45</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem
