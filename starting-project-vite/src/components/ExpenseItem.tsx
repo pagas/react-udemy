7445bed7c41605954dd6b5c1d77c9dfe15523b07
Paulius G <paulius@2simple.com>
2023-10-12 07:55:41 +0100

    format expense date

--- starting-project-vite/src/components/ExpenseItem.tsx
+++ starting-project-vite/src/components/ExpenseItem.tsx
@@ -6,10 +6,21 @@ interface ExpensesItemProps {
     amount: number
 }
 
-function ExpenseItem({date, title, amount}: ExpensesItemProps) {
+function ExpenseItem({ date, title, amount }: ExpensesItemProps) {
+    const month = firstLetterUppercase(date.toLocaleString(undefined, { month: 'long' }));
+    const day = date.toLocaleString(undefined, { day: '2-digit' });
+
+    function firstLetterUppercase(str: string) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
     return (
         <div className='expense-item'>
-            <div>{date.toDateString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{date.getFullYear()}</div>
+                <div>{day}</div>
+            </div>
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>${amount}</div>
