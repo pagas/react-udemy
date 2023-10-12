7ced66b28b425895d5bf3a27b14b4fe308acdf85
Paulius G <paulius@2simple.com>
2023-10-12 11:14:48 +0100

    add expense date component

---
+++ starting-project-vite/src/components/ExpenseDate.tsx
@@ -0,0 +1,25 @@
+import './ExpenseDate.css'
+
+interface ExpenseDateProps {
+    date: Date
+}
+
+function ExpenseDate({ date }: ExpenseDateProps) {
+    const locale = undefined; // use default browser locale
+    const month = date.toLocaleString(locale, { month: 'long' });
+    const day = date.toLocaleString(locale, { day: '2-digit' });
+
+    function firstLetterUppercase(str: string) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+    return (
+        <div className='expense-date'>
+            <div className='expense-date__month'>{firstLetterUppercase(month)}</div>
+            <div className='expense-date__year'>{date.getFullYear()}</div>
+            <div className='expense-date__day'>{day}</div>
+        </div >
+    )
+}
+
+export default ExpenseDate
--- starting-project-vite/src/components/ExpenseItem.tsx
+++ starting-project-vite/src/components/ExpenseItem.tsx
@@ -1,3 +1,4 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 interface ExpensesItemProps {
@@ -7,20 +8,9 @@ interface ExpensesItemProps {
 }
 
 function ExpenseItem({ date, title, amount }: ExpensesItemProps) {
-    const month = firstLetterUppercase(date.toLocaleString(undefined, { month: 'long' }));
-    const day = date.toLocaleString(undefined, { day: '2-digit' });
-
-    function firstLetterUppercase(str: string) {
-        return str.charAt(0).toUpperCase() + str.slice(1);
-    }
-
     return (
         <div className='expense-item'>
-            <div>
-                <div>{month}</div>
-                <div>{date.getFullYear()}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={date} />
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>${amount}</div>
