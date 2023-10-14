7d8bec2527d53f45f2241e21d882341d65686f8e
Paulius G <paulius@2simple.com>
2023-10-14 16:29:17 +0100

    dynamically add invalid class

--- styling-components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ styling-components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -28,14 +28,10 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+      <div className={`form-control ${isValid ? '': 'invalid'}`} >
+        <label>Course Goal</label>
         <input 
             type="text" 
-            style={{
-              borderColor: !isValid ? 'red' : '#ccc',
-              background: !isValid ? 'salmon' : 'transparent'
-            }}
             value={enteredValue}
             onChange={goalInputChangeHandler} />
       </div>
