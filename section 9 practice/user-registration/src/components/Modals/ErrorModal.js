e058ee1e5204013ce8513eb2e347ae422d04c99c
Paulius G <paulius@2simple.com>
2023-10-15 21:04:26 +0100

    backdrop click close the modal

--- section 9 practice/user-registration/src/components/Modals/ErrorModal.js
+++ section 9 practice/user-registration/src/components/Modals/ErrorModal.js
@@ -5,9 +5,9 @@ import styles from './Modal.module.css';
 const ErrorModal = (props) => {
     const {  message, title, onClose } = props;
     const _title = title || 'Invalid Input';
-    
+
     return (
-        <div className={`${styles.modal} ${styles['display-block']}`}>
+        <div className={`${styles.modal} ${styles['display-block']}`} onClick={onClose}>
             <Card className={styles['modal-main']}>
                 <div className={styles['modal-header']}>
                     <h5 className={styles['modal-title']}>{_title}</h5>
