bdb3626a5ba7b31dad413334068b110aaac223e5
Paulius G <paulius@2simple.com>
2023-10-15 21:51:26 +0100

    use portals for modal and backdrop

--- section 9 practice/user-registration/src/components/Modals/ErrorModal.js
+++ section 9 practice/user-registration/src/components/Modals/ErrorModal.js
@@ -1,27 +1,45 @@
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import styles from './Modal.module.css';
+import ReactDOM from 'react-dom';
+
+const Backdrop = props => {
+    return <div className={styles.backdrop} onClick={props.onClose} />
+}
+
+const ModalOverlay = props => {
+    const { message, title, onClose } = props;
+    const _title = title || 'Invalid Input';
+    return (
+        <Card className={styles['modal-main']}>
+            <div className={styles['modal-header']}>
+                <h5 className={styles['modal-title']}>{_title}</h5>
+            </div>
+            <div className={styles['modal-body']}>
+                {message}
+            </div>
+            <div className={styles['modal-footer']}>
+                <Button type="button" onClick={onClose}>
+                    Okay
+                </Button>
+            </div>
+        </Card>
+    )
+}
 
 const ErrorModal = (props) => {
-    const {  message, title, onClose } = props;
-    const _title = title || 'Invalid Input';
 
     return (
-        <div className={`${styles.modal} ${styles['display-block']}`} onClick={onClose}>
-            <Card className={styles['modal-main']}>
-                <div className={styles['modal-header']}>
-                    <h5 className={styles['modal-title']}>{_title}</h5>
-                </div>
-                <div className={styles['modal-body']}>
-                    {message}
-                </div>
-                <div className={styles['modal-footer']}>
-                    <Button type="button" onClick={onClose}>
-                        Okay
-                    </Button>
-                </div>
-            </Card>
-        </div>
+        <>
+            {ReactDOM.createPortal(
+                <Backdrop {...props} />,
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay  {...props} />,
+                document.getElementById('overlay-root')
+            )}
+        </>
     );
 
 }
