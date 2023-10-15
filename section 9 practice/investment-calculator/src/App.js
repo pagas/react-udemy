bfaa2f0e7dedc3e14440cc077c165c8ff4cfd5ef
Paulius G <paulius@2simple.com>
2023-10-15 11:41:49 +0100

    move calculate function to app component

--- section 9 practice/investment-calculator/src/App.js
+++ section 9 practice/investment-calculator/src/App.js
@@ -6,9 +6,38 @@ import Header from './components/UI/Header';
 function App() {
   const [calcResults, setCalcResults] = useState(null)
 
-  const calculateHandler = (results) => {
-    console.log('result', results)
-    setCalcResults(results);
+  const calculateHandler = (userInput) => {
+
+    // Should be triggered when form is submitted
+    // You might not directly want to bind it to the submit event on the form though...
+
+    const yearlyData = []; // per-year results
+
+    let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
+    const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
+    const expectedReturn = +userInput['expected-return'] / 100;
+    const duration = +userInput['duration'];
+    let totalContribution = 0;
+    let totalInterest = 0;
+
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      totalContribution += yearlyContribution;
+      totalInterest += yearlyInterest;
+      yearlyData.push({
+        // feel free to change the shape of the data pushed to the array!
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+        totalContribution: totalContribution,
+        totalInterest: totalInterest
+      });
+    }
+
+    setCalcResults(yearlyData);
   }
 
   return (
--- section 9 practice/investment-calculator/src/components/Calculator/CalculatorForm.js
+++ section 9 practice/investment-calculator/src/components/Calculator/CalculatorForm.js
@@ -3,10 +3,10 @@ import styles from './CalculateForm.module.css';
 
 
 const initialUserInput = {
-    'current-savings': 0,
-    'yearly-contribution': 0,
-    'expected-return': 0,
-    duration: 0
+    'current-savings': 10000,
+    'yearly-contribution': 1200,
+    'expected-return': 7,
+    duration: 10
 }
 
 const CalculatorForm = (props) => {
@@ -14,39 +14,7 @@ const CalculatorForm = (props) => {
 
     const calculateHandler = (e) => {
         e.preventDefault();
-        // Should be triggered when form is submitted
-        // You might not directly want to bind it to the submit event on the form though...
-
-        const yearlyData = []; // per-year results
-
-        let currentSavings = +userInput['current-savings']; // feel free to change the shape of this input object!
-        const yearlyContribution = +userInput['yearly-contribution']; // as mentioned: feel free to change the shape...
-        const expectedReturn = +userInput['expected-return'] / 100;
-        const duration = +userInput['duration'];
-        let totalContribution = 0;
-        let totalInterest = 0;
-
-        // The below code calculates yearly results (total savings, interest etc)
-        for (let i = 0; i < duration; i++) {
-            const yearlyInterest = currentSavings * expectedReturn;
-            currentSavings += yearlyInterest + yearlyContribution;
-            totalContribution += yearlyContribution;
-            totalInterest += yearlyInterest;
-            yearlyData.push({
-                // feel free to change the shape of the data pushed to the array!
-                year: i + 1,
-                yearlyInterest: yearlyInterest,
-                savingsEndOfYear: currentSavings,
-                yearlyContribution: yearlyContribution,
-                totalContribution: totalContribution,
-                totalInterest: totalInterest
-            });
-        }
-
-    
-        props.onCalculate(yearlyData);
-
-        // do something with yearlyData ...
+        props.onCalculate(userInput);
     };
 
     const changeHandler = (e, inputField) => {
