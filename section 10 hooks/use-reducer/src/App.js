18b23e5626a4ab60a61d9eb97cbe46364cfeacf1
Paulius G <paulius@2simple.com>
2023-10-18 11:43:51 +0100

    user reducer practice project

---
+++ section 10 hooks/use-reducer/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  console.log('app rendering')
+  // is executed only once, not everytime is rerendered
+  useEffect(() => {
+    console.log('use effect in effect')
+    const storedLoggendInfo = localStorage.getItem('isLoggedIn');
+    if (storedLoggendInfo === '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
