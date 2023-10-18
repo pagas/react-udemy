0f3afad71dd252a915191d891885adc4498f1454
Paulius G <paulius@2simple.com>
2023-10-18 12:55:44 +0100

    use context project

---
+++ section 10 hooks/use-context/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  console.log('app rendering')
+  // is executed only once, not everytime is rerendered
+  useEffect(() => {
+    console.log('use effect in effect')
+    const storedLoggendInfo = localStorage.getItem('isLoggedIn');
+    if (storedLoggendInfo === '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ 
+        isLoggedIn: isLoggedIn,
+        logout: logoutHandler
+    }}>
+      <MainHeader />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </AuthContext.Provider>
+  );
+}
+
+export default App;
---
+++ section 10 hooks/use-context/src/components/MainHeader/Navigation.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+
+import classes from './Navigation.module.css';
+import AuthContext from '../../store/auth-context';
+
+const Navigation = (props) => {
+  const { isLoggedIn, logout } = useContext(AuthContext);
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li>
+            <button onClick={logout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
