194ca9b6358f30173495956f04cd73f16bfc5f6d
Paulius G <paulius@2simple.com>
2023-10-18 22:07:00 +0100

    use reducer to create cart storage

--- food-order-app-practice/src/store/CartProvider.js
+++ food-order-app-practice/src/store/CartProvider.js
@@ -1,29 +1,55 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_ITEM': {
+            let updateItems = [...state.items, action.item];
+            let updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+            return { items: updateItems, totalAmount: updatedTotalAmount }
+        } case 'REMOVE_ITEM': {
+            const foundItem = state.items.find(item => item.id === action.id)
+            const updateItems = state.items.filter(item => item.id !== action.id)
+            let updatedTotalAmount = state.totalAmount - foundItem.price * foundItem.amount;
+
+            return { items: updateItems, totalAmount: updatedTotalAmount }
+        } default:
+    }
+
+    return defaultCartState;
+}
+
+const totalAmount = (items) => items.reduce((current, item) => {
+    return current + item.amount;
+}, 0)
+
+
 const CartProvider = (props) => {
-    const [items, setItems] = useState([]);
+    const [cartState, dispatchCart] = useReducer(cartReducer, defaultCartState);
 
+    const addItemHandler = (item) => {
+        dispatchCart({ type: 'ADD_ITEM', item })
+    }
 
-    const addItem = (item) => {
-        setItems(prev => {
-            return [...prev, item]
-        });
-    };
+    const removeItemHandler = (id) => {
+        dispatchCart({ type: 'REMOVE_ITEM', id })
+    }
 
-    const removeItem = (itemId) => {
-        setItems(prev => {
-            return prev.filter(item => item.id !== itemId)
-        })
-    };
+    const cartContext = {
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
+        addItem: addItemHandler,
+        removeItem: removeItemHandler
+    }
 
     return (
-        <CartContext.Provider value={{
-            items,
-            addItem,
-            totalAmount: 0,
-            removeItem
-        }}>
+        <CartContext.Provider value={cartContext}>
             {props.children}
         </CartContext.Provider>
     )
