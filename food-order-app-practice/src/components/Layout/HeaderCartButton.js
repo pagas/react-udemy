5a0d54919bcbeba8058001f8dfc9e40b27bc8abd
Paulius G <paulius@2simple.com>
2023-10-19 02:26:11 +0100

    cart button animation

--- food-order-app-practice/src/components/Layout/HeaderCartButton.js
+++ food-order-app-practice/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,32 @@
 import styles from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 
 
 const HeaderCartButton = ({ children, onClick }) => {
+    const [animationRunning, setAnimationRunning] = useState(false);
     const { items } = useContext(CartContext);
     const totalAmount = items.reduce((current, item) => {
         return current + item.amount;
     }, 0)
 
+    const btnStyles = `${styles.button} ${animationRunning ? styles.bump : ''}`;
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setAnimationRunning(true)
+        const timer = setTimeout(() => {
+            setAnimationRunning(false)
+        }, 300);
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items])
+
     return (
-        <button className={styles.button} onClick={onClick}>
+        <button className={btnStyles} onClick={onClick}>
             <span className={styles.icon}><CartIcon /></span>
             <span>Your cart</span>
             <span className={styles.badge}>{totalAmount}</span>
