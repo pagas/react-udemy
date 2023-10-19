017d8c86b62c0866ab0ecf84fc4a933c01bcb477
Paulius G <paulius@2simple.com>
2023-10-19 01:15:31 +0100

    cart show total and add icon style and layout

--- food-order-app-practice/src/components/Cart/Cart.js
+++ food-order-app-practice/src/components/Cart/Cart.js
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
 import Modal from '../UI/Modal';
 import styles from './Cart.module.css';
 import CartItem from './CartItem';
+import CartContext from '../../store/cart-context';
 
 const Cart = ({ onClose }) => {
-    const cartItems = [{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }]
+    const {items, totalAmount} = useContext(CartContext);
+    const hasItems = items.length > 0;
+
     const closeHandler = () => {
         onClose()
     }
@@ -11,17 +15,17 @@ const Cart = ({ onClose }) => {
     return (
         <Modal onClose={closeHandler}>
             <ul className={styles['cart-items']}>
-                {cartItems.map(cartItem => (
+                {items.map(cartItem => (
                     <CartItem {...cartItem} />
                 ))}
             </ul>
             <div className={styles.total}>
                 <span>Total Amount</span>
-                <span>35.34</span>
+                <span>{`$${totalAmount.toFixed(2)}`}</span>
             </div>
             <div className={styles.actions}>
                 <button className={styles['buttons--alt']} onClick={closeHandler}>Close</button>
-                <button className={styles.button}>Order</button>
+                {hasItems && <button className={styles.button}>Order</button>}
             </div>
         </Modal>
     )
