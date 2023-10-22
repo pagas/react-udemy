c66bd36a682ddc32c9cc44c10d9f4cb28748a5dd
Paulius G <paulius@2simple.com>
2023-10-22 22:08:13 +0100

    ability to fetch meals

--- section 18 practice http forms/src/components/Meals/AvailableMeals.js
+++ section 18 practice http forms/src/components/Meals/AvailableMeals.js
@@ -1,49 +1,64 @@
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
+import useHttp from '../../hooks/use-http';
+import { useEffect, useState, useCallback } from 'react'
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+  const [meals, setMeals] = useState([]);
+
+  const { isLoading, error, sendRequest } = useHttp({
+    url: process.env.REACT_APP_SERVER_URL + 'x'
+  });
+
+  const fetchMeal = useCallback(async () => {
+    const data = await sendRequest();
+
+    if (!data) {
+      return;
+    }
+    const loadedMeal = [];
+
+    for (const [id, meal] of Object.entries(data)) {
+      loadedMeal.push({ id: id, ...meal })
+    }
+    setMeals(loadedMeal)
+  }, [sendRequest])
+
+
+  useEffect(() => {
+    fetchMeal();
+  }, [fetchMeal])
+
+
+  let content = <p>No meals found.</p>;
+
+  if (isLoading) {
+    content = <p>Loading...</p>
+  }
+
+  if (error) {
+    <p>{error}</p>
+  }
+
+  if (meals.length > 0)
+    content = (<ul>
+      {meals.map((meal) => (
+        <MealItem
+          key={meal.id}
+          id={meal.id}
+          name={meal.name}
+          description={meal.description}
+          price={meal.price}
+        />
+      ))}
+    </ul>);
 
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        {content}
       </Card>
     </section>
   );
