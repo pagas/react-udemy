ed3d5b1baf319fa25bd26423ed7ec5ff6883cc42
Paulius G <paulius@2simple.com>
2023-10-29 19:58:17 +0000

    create auth slice and use for login log out

--- section 19 Redux/simple/src/store/index.js
+++ section 19 Redux/simple/src/store/index.js
@@ -1,10 +1,8 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit'
 
-const initialState = { counter: 0, showCounter: true }
-
 const counterSlice = createSlice({
     name: 'counter',
-    initialState,
+    initialState: { counter: 0, showCounter: true },
     reducers: {
         increment(state, action) {
             state.counter = state.counter + (action.payload ? action.payload : 1);
@@ -18,10 +16,27 @@ const counterSlice = createSlice({
     }
 });
 
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: { isAuthenticated: false },
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
+        }
+    }
+});
+
 
 const store = configureStore({
-    reducer: counterSlice.reducer
+    reducer: {
+        counter: counterSlice.reducer,
+        auth: authSlice.reducer
+    }
 })
 
 export const couterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
