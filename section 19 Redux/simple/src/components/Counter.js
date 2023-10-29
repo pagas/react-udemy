2f2d913ff3055610876dbc412723d5291eaecca1
Paulius G <paulius@2simple.com>
2023-10-29 13:54:07 +0000

    dispatch action store

--- section 19 Redux/simple/src/components/Counter.js
+++ section 19 Redux/simple/src/components/Counter.js
@@ -1,20 +1,29 @@
 import classes from './Counter.module.css';
 import store from '../store';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
   // auto add and clear store subscription
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   const toggleCounterHandler = () => {
     store.dispatch({ type: 'add' })
   };
 
+  const addCounterHandler = () => dispatch({ type: 'add' });
+  const reduceCounterHandler = () => dispatch({ type: 'reduce' });
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <div>
+        <button onClick={addCounterHandler}>Add</button>
+        <button onClick={reduceCounterHandler}>Reduce</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle</button>
+
     </main>
   );
 };
